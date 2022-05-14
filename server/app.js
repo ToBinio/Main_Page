687adde6592c253ebf62eac5528a3428c1897a0c
ToBinio <tobias.frischmann1@gmail.com>
2022-05-14 17:48:20 +0200

    basic Express page

---
+++ server/app.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var path = require("path");
+var express = require("express");
+var app = express();
+app.use(express.static("../client/public"));
+app.get("", function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
+app.listen(3000, function () {
+    console.log("Server started on Port: 300");
+});
---
+++ server/app.ts
@@ -0,0 +1,14 @@
+import * as path from "path";
+
+const express = require("express")
+const app = express()
+
+app.use(express.static("../client/public"))
+
+app.get("", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/public/index.html"))
+})
+
+app.listen(3000, () => {
+    console.log("Server started on Port: 300");
+})
